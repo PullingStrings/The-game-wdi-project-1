3f18642f93bd5faebe6202a896f1a8facea4988c
PullingStrings <drammatik@gmail.com>
2017-08-30 15:29:44 +0100

    working with background

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,50 @@
 $(() => {
   const $ballons = $('.ballon');
-  let greenCount = 0;
   const $startBtn = $('.startBtn');
   const $resetBtn = $('.resetBtn');
   const $result = $('.result');
   const $points = $('.points');
   let score = 0;
-
+  let greenCount = 0;
 
   // when you click on the start button it will start game
   $startBtn.on('click',() =>{
     greenCount = 0;
     $ballons.each((i, ballon) => {
-      $(ballon).animate(({top: '-20%'}), Math.floor(Math.random() * 6000) + 6000);
+      $(ballon).animate(({top: '-20%'}), Math.floor(Math.random() * 7000) + 7000)
+        .addClass('animated pulse infinite');
+      // if ballon is clicked dont run
+      // for(i = greenCount;i <= 4;i++){
+      $(ballon).animate({top: '100%'}, 0);
+      //   if ($ballons.hasClass('green') === $(ballon).animate(({top: '-20%'}))){
+      //     $(ballon).animate(({top: '100%'}, Math.floor(Math.random() * 7000) + 7000));
+      //   }
+      // }
+
+      // wait for all green ballons to reach top -20% before you run the for loop in the startBtn
+
     });
   });
+
+  // audio for pop ballon
+
+  function blopAudio(){
+    const audio = ('/audio/blop.wav');
+    audio.play();
+  }
+  blopAudio();
+
   // the win and lose function with the score
   $ballons.on('click', (e) => {
     if($(e.target).hasClass('red')) {
       if(greenCount === 4) {
-        $result.text('You Win');
+        $result.text('You Win').addClass('animated shake');
         score++;
+        $ballons.stop();
+        $ballons.css({top: '100%'});
+
       } else {
-        $result.text('You Lose');
+        $result.text('You Lose').addClass('animated shake');
         score--;
         $ballons.stop();
         $ballons.css({top: '100%'});
@@ -33,23 +55,17 @@ $(() => {
     if($(e.target).hasClass('green')) greenCount++;
     $(e.target).stop();
     $(e.target).css({top: '100%'});
+
   });
 
   // this button will reset the game
   $resetBtn.on('click',() =>{
     $resetBtn.html('Reset');
     $result.text('');
-    $points.text('');
+    $points.text('0');
     $ballons.stop().css({top: '100%'});
-    score.text('0');
+    score = 0;
   });
 
-  // if red reaches -20% send ballon back to top 100%
-  // const $startOver = $ballons.animate({top: '-20%'});
-  // $startOver.on('animationend', (e) =>{
-  //   if($(e.target).hasClass('red')){
-  //     $ballons.stop().css({top: '100%'});
-  //   }
-  // });
 
 }); // end of DOMContentLoaded
--- tasks/watch.js
+++ tasks/watch.js
@@ -5,8 +5,9 @@ const watch = () => {
   gulp.watch(`${config.src.scss}${config.selectors.scss}`, ['build-css']);
   gulp.watch(`${config.src.js}${config.selectors.js}`, ['build-js']);
   gulp.watch(`${config.src.images}${config.selectors.images}`, ['build-images']);
+  gulp.watch(`${config.src.audio}${config.selectors.audio}`, ['build-audio']);
   gulp.watch(`${config.srcDir}${config.main.index}`, ['build-index']);
 };
 
 gulp.task('watch', watch);
-module.exports = watch;
+module.exports = watch;exports = watch;
